740641e05d43d75119e6e253e5cd26fcf2b251c4
BOURBON21 <kdy00sky@ut.ac.kr>
2025-07-24 18:02:00 +0900

    aa
    aa

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "8.10.1" apply false
+    id("com.android.library") version "8.10.1" apply false
+    kotlin("android") version "1.9.10" apply false // 예시
+}
